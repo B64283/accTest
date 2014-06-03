66a907119c73a0b140e3aa040da3039498dd4def
B64283 <mattdarke@fullsail.edu>
2014-06-03 01:26:53 -0700

    reset mistakes

---
+++ accTest/TwitterUserPost.h
@@ -0,0 +1,13 @@
+//
+//  TwitterUserPost.h
+//  accTest
+//
+//  Created by Matthew Darke on 6/3/14.
+//  Copyright (c) 2014 Matthew Darke. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TwitterUserPost : NSObject
+
+@end
---
+++ accTest/TwitterUserPost.m
@@ -0,0 +1,13 @@
+//
+//  TwitterUserPost.m
+//  accTest
+//
+//  Created by Matthew Darke on 6/3/14.
+//  Copyright (c) 2014 Matthew Darke. All rights reserved.
+//
+
+#import "TwitterUserPost.h"
+
+@implementation TwitterUserPost
+
+@end
